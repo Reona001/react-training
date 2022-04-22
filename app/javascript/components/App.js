72a5a718f626ba01638efadd811532768438753e
reona kasuya <reon.kasuya@gmail.com>
2022-04-22 12:43:22 +0900

    simple component linked with index.html

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>Hello World</div>
+    // https://www.youtube.com/watch?v=5F_JUvPq410&ab_channel=zayne
+    )
+  }
+}
+
+export default App
